9969e43fdec780c311382f8d3e5caee7f51e4b58
Robert Forest <rvforest@gmail.com>
2025-02-22 22:56:47 -0500

    Refactor with focus on config

--- src/pi_spin/main.py
+++ src/pi_spin/main.py
@@ -1,8 +1,8 @@
 import logging
 
-from pi_spin import raspberrypi
-from pi_spin.database import db
-from pi_spin.config import config
+from pi_spin.config import load_config
+from pi_spin.raspberrypi import RaspberryPi
+from pi_spin.database import Database
 from pi_spin.workout import Workout
 
 logger = logging.getLogger(__name__)
@@ -10,27 +10,45 @@ logger = logging.getLogger(__name__)
 
 def start_smart_bike():
     """Start smart bike program."""
+    _setup_logging()
     logger.info("Program starting...")
+    db = Database.from_config()
 
+    raspberrypi = RaspberryPi.from_config()
     raspberrypi.setup()
     try:
-        _main_loop()
+        _main_loop(db, raspberrypi)
     except KeyboardInterrupt:
         logger.info("Program terminated")
     except Exception as e:
         logger.exception(e)
     finally:
-        _cleanup()
+        logger.debug("Cleaning up")
+        raspberrypi.cleanup()
+        db.cleanup()
 
 
-def _main_loop():
+def _main_loop(db, raspberrypi):
     while True:
-        raspberrypi.wait_for_inputs([config["START_STOP_BUTTON_PIN"]])
-        exercise = Workout()
+        raspberrypi.wait_for_start_button()
+        exercise = Workout(db, raspberrypi)
+        exercise.start_workout()
         exercise.log_pedal_strokes()
+        exercise.end_workout()
 
 
-def _cleanup():
-    logger.debug("Cleaning up")
-    raspberrypi.cleanup()
-    db.cleanup()
+def _setup_logging():
+    config = load_config("logging.yaml")
+    handlers = []
+    if config["log_file"] is not None:
+        handlers.append(logging.FileHandler(config["log_file"]))
+    if config["log_to_console"] is not None:
+        handlers.append(logging.StreamHandler())
+
+    logging.basicConfig(
+        level=getattr(logging, config["log_level"]),
+        format=config["format"],
+        handlers=handlers,
+    )
+
+    logging.debug(config)
